cdf8005a241da3de6e14a9b0fe166480c58cd54f
João Victor Figueiredo <fig.joao.victor@gmail.com>
2022-04-27 20:25:50 -0300

    Project Urna

---
+++ etapas.js
@@ -0,0 +1,51 @@
+let etapas =[
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '48111',
+                nome: 'Salvador da Rima',
+                partido: 'PJL',
+                fotos:[
+                {url: '1.png', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero: '65612',
+                nome: 'Pesqueiro kk',
+                partido: 'ABD',
+                fotos: [
+                {url: '2.png', legenda: 'Vereador', small: false}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2, 
+        candidatos: [
+            {
+                numero: '55',
+                nome: 'Ferroveiro',
+                partido: 'Capita',
+                vice: 'Cic',
+                fotos:[
+                {url: '01.png', legenda: 'Prefeito'},
+                {url: '01-1.webp', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '85',
+                nome: 'Cacereiro',
+                partido: 'Sergi',
+                vice: 'Coque',
+                fotos:[
+                {url: '02.png', legenda: 'Prefeito'},
+                {url: '02-1.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+        
+    }
+]
